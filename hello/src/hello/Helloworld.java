bdc102fafa6b6454bb56e6f365eb681b9946eff3
yunsungtyeol <wkdal704@gmail.com>
2019-10-31 09:46:52 +0900

    add hotfix

--- hello/src/hello/Helloworld.java
+++ hello/src/hello/Helloworld.java
@@ -4,7 +4,8 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello world");
+		System.out.println("hotfix code wirte");
 	}
 
 }
